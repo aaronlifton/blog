f2552aedc89ec8262473c39cfb0098c71f932761
Aaron Lifton <aaronlifton@gmail.com>
2024-03-04 23:15:41 -0500

    remove console.logs; move scripts

--- src/pages/images/[...slug].ts
+++ src/pages/images/[...slug].ts
@@ -3,32 +3,31 @@ import fs from "fs/promises";
 import path from "path";
 
 export async function getStaticPaths() {
-  const files: string[] = await fs.readdir("src/content/blog/assets/");
+	const files: string[] = await fs.readdir("src/content/blog/assets/");
 
-  return files.map((fileName) => {
-    return {
-      params: {
-        slug: fileName,
-      },
-    };
-  });
+	return files.map((fileName) => {
+		return {
+			params: {
+				slug: fileName,
+			},
+		};
+	});
 }
 
 export const GET: APIRoute = async ({ props, params }) => {
-  console.log({ props, params });
-  // Define the path to your image file
-  const imagePath = path.join(
-    process.cwd(),
-    "/src/content/blog/assets/screenshots/nvim-no-unused-vars.png",
-  );
+	// Define the path to your image file
+	const imagePath = path.join(
+		process.cwd(),
+		"/src/content/blog/assets/screenshots/nvim-no-unused-vars.png",
+	);
 
-  // Read the image file
-  const imageBuffer = await fs.readFile(imagePath);
+	// Read the image file
+	const imageBuffer = await fs.readFile(imagePath);
 
-  // Return the image as a response
-  return new Response(imageBuffer, {
-    headers: {
-      "Content-Type": "image/png",
-    },
-  });
+	// Return the image as a response
+	return new Response(imageBuffer, {
+		headers: {
+			"Content-Type": "image/png",
+		},
+	});
 };
--- src/plugins/remark-reading-time.mjs
+++ src/plugins/remark-reading-time.mjs
@@ -1,27 +1,11 @@
-import { toString as mdastToString } from 'mdast-util-to-string'
-import getReadingTime from 'reading-time'
+import { toString as mdastToString } from "mdast-util-to-string";
+import getReadingTime from "reading-time";
 
 export const remarkReadingTime = (tree, file) => {
 	const textOnPage = mdastToString(tree);
 	const readingTime = getReadingTime(textOnPage);
-	console.log("file", file);
-	console.log("readingTime", readingTime);
 	// readingTime.text will give us minutes read as a friendly string,
 	// i.e. "3 min read"
 	if (!file) return;
 	file.data.astro.frontmatter.minutesRead = readingTime.text;
 };
-// import { toString as mdastToString } from 'mdast-util-to-string'
-// import getReadingTime from 'reading-time'
-// import type { Plugin } from 'unified'
-// import { type Root } from '@types/mdast'
-//
-// export const remarkReadingTime: Plugin<[], Root> = () => {
-// 	return function (tree, file) {
-// 		const textOnPage = mdastToString(tree)
-// 		const readingTime = getReadingTime(textOnPage)
-//
-// 		// @ts-expect-error FIXME: find a way to inject frontmatter safely
-// 		file.data.astro.frontmatter.minutesRead = readingTime.minutes
-// 	}
-// }
