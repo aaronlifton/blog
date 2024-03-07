c19dfba158ad4f30e46e937e45213806fc83367c
Aaron Lifton <aaronlifton@gmail.com>
2024-03-07 15:50:23 -0500

    create Dockerfile

---
+++ bin/processImages.mjs
@@ -0,0 +1,54 @@
+import { readdirSync, mkdirSync, existsSync } from "fs";
+import sharp from "sharp";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const size = {
+	width: 250,
+	height: 100,
+};
+const processImages = () => {
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = path.dirname(__filename);
+
+	const srcDir = path.resolve(__dirname, "../");
+	const directoryPath = path.join(srcDir, "src/content/blog/assets");
+	const relativeOutputDirectory = "src/content/blog/assets/og-images";
+	const outputDirectory = path.join(srcDir, relativeOutputDirectory);
+	const dirExists = existsSync(relativeOutputDirectory);
+	if (!dirExists) {
+		mkdirSync(`./${relativeOutputDirectory}`);
+	}
+	console.log(`Output directory: ${outputDirectory}`);
+	readdirSync(directoryPath, (err, files) => {
+		if (err) {
+			return console.log(`Unable to scan directory: ${err}`);
+		}
+
+		for (const file of files) {
+			if (path.extname(file) === ".jpg" || path.extname(file) === ".png") {
+				sharp(`${directoryPath}/${file}`)
+					.resize(size.width, size.height, {
+						fit: "cover",
+						position: "center",
+					})
+					.toFile(`${outputDirectory}/${file}`, (err, _info) => {
+						if (err) {
+							console.log(err);
+						} else {
+							console.log(`Image ${file} ->\t ${size.width}x${size.height}.`);
+						}
+					});
+			}
+		}
+	});
+};
+
+export default {
+	name: "custom-image-resizer-integration",
+	hooks: {
+		"astro:config:setup": () => {
+			processImages();
+		},
+	},
+};
