ab815dc8904316bde7fc1caac0e8f947a208cf1d
Aaron Lifton <aaronlifton@gmail.com>
2024-03-13 16:14:56 -0400

    improvements

--- src/pages/api/blog/views/[slug].json.ts
+++ src/pages/api/blog/views/[slug].json.ts
@@ -2,22 +2,23 @@ import type { APIRoute } from "astro";
 import type { Value } from "@libsql/core/api";
 import { getCollection } from "astro:content";
 import { getViewsBySlug } from "../../../../services/turso.js";
-import { getViewsBySlug as inMemoryGetViews } from "../../../../services/memory.js";
+// import { getViewsBySlug as inMemoryGetViews } from "../../../../services/memory.js";
 
-const getViewsFn = import.meta.env.PROD ? getViewsBySlug : inMemoryGetViews;
+// const getViewsFn = import.meta.env.PROD ? getViewsBySlug : inMemoryGetViews;
+const getViewsFn = getViewsBySlug;
 
 export const prerender = true;
 export async function getStaticPaths() {
-	const posts = await getCollection("blog");
-	return posts.map((post) => ({
-		params: { slug: post.slug },
-	}));
+  const posts = await getCollection("blog");
+  return posts.map((post) => ({
+    params: { slug: post.slug },
+  }));
 }
 
 export const GET: APIRoute = async ({ params, request: _ }) => {
-	let views: Value | number = 0;
-	if (params.slug) {
-		views = await getViewsFn(params.slug);
-	}
-	return new Response(JSON.stringify({ views }));
+  let views: Value | number = 0;
+  if (params.slug) {
+    views = await getViewsFn(params.slug);
+  }
+  return new Response(JSON.stringify({ views }));
 };
--- src/services/turso.js
+++ src/services/turso.js
@@ -4,111 +4,113 @@ import prisma from "./db";
 import { ErrorModel } from "$prisma/zod/error";
 
 export const client = createClient({
-	url: import.meta.env.TURSO_DB_URL,
-	authToken: import.meta.env.TURSO_DB_AUTH_TOKEN,
+  url: import.meta.env.TURSO_DB_URL,
+  authToken: import.meta.env.TURSO_DB_AUTH_TOKEN,
 });
 
 let isLocal;
 const isLocalhost = () => {
-	if (isLocal !== undefined) {
-		return isLocal;
-	}
-	isLocal = os.arch() === "arm64";
-	return isLocal;
+  if (isLocal !== undefined) {
+    return isLocal;
+  }
+
+  // Mac M2 Mac Mini has 12 cores
+  isLocal = os.arch() == "arm64" && os.cpus().length == 12;
+  return isLocal;
 };
 export const getViewsBySlug = async (slug) => {
-	if (isLocalhost() || !slug) {
-		return 0;
-	}
+  if (isLocalhost() || !slug) {
+    return 0;
+  }
 
-	const upsert = async () => {
-		const updated = prisma.blogPostMeta.upsert({
-			where: {
-				postSlug: slug,
-			},
-			create: {
-				postSlug: slug,
-				numViews: initialViewCount,
-			},
-			update: {
-				numViews: blogPost.numViews + 1,
-			},
-			select: {
-				postSlug: true,
-				numViews: true,
-			},
-		});
-	};
+  const upsert = async () => {
+    const updated = prisma.blogPostMeta.upsert({
+      where: {
+        postSlug: slug,
+      },
+      create: {
+        postSlug: slug,
+        numViews: initialViewCount,
+      },
+      update: {
+        numViews: blogPost.numViews + 1,
+      },
+      select: {
+        postSlug: true,
+        numViews: true,
+      },
+    });
+  };
 
-	try {
-		const initialViewCount = 0;
-		let blogPost = await prisma.blogPostMeta.findUnique({
-			// Since where only contains one field, which is unique, prisma will use a
-			// DB upsert, which avoids unique constraint violations
-			// and eschews the need for a transaction.
-			// See: https://www.prisma.io/docs/orm/reference/prisma-client-reference#database-upserts
-			where: {
-				postSlug: slug,
-			},
-			select: {
-				postSlug: true,
-				numViews: true,
-			},
-		});
-		if (!blogPost) {
-			return initialViewCount;
-		}
-		blogPost = upsert();
-		return blogPost.numViews;
-	} catch (e) {
-		if (e.code === "P2002") {
-			// https://www.prisma.io/docs/concepts/components/prisma-client/handling-errors
-			// A unique constraint was violated
-			// This means that a unique constraint was violated and the transaction was rolled back
-			console.error("Unique constraint violation", e);
-			return 0;
-		}
-		console.error(e);
-		return 0;
-	}
+  try {
+    const initialViewCount = 0;
+    let blogPost = await prisma.blogPostMeta.findUnique({
+      // Since where only contains one field, which is unique, prisma will use a
+      // DB upsert, which avoids unique constraint violations
+      // and eschews the need for a transaction.
+      // See: https://www.prisma.io/docs/orm/reference/prisma-client-reference#database-upserts
+      where: {
+        postSlug: slug,
+      },
+      select: {
+        postSlug: true,
+        numViews: true,
+      },
+    });
+    if (!blogPost) {
+      return initialViewCount;
+    }
+    blogPost = upsert();
+    return blogPost.numViews;
+  } catch (e) {
+    if (e.code === "P2002") {
+      // https://www.prisma.io/docs/concepts/components/prisma-client/handling-errors
+      // A unique constraint was violated
+      // This means that a unique constraint was violated and the transaction was rolled back
+      console.error("Unique constraint violation", e);
+      return 0;
+    }
+    console.error(e);
+    return 0;
+  }
 };
 
 export const getViews = async () => {
-	// return a hash of views by slug
-	if (isLocalhost()) {
-		return {};
-	}
-	try {
-		const results = await prisma.blogPostMeta.findMany({
-			select: {
-				postSlug: true,
-				numViews: true,
-			},
-		});
-		const views = {};
-		for (const result of results) {
-			views[result.postSlug] = result.numViews;
-		}
-		return views;
-	} catch (e) {
-		console.error(e);
-		return {};
-	}
+  // return a hash of views by slug
+  if (isLocalhost()) {
+    return {};
+  }
+  try {
+    const results = await prisma.blogPostMeta.findMany({
+      select: {
+        postSlug: true,
+        numViews: true,
+      },
+    });
+    const views = {};
+    for (const result of results) {
+      views[result.postSlug] = result.numViews;
+    }
+    return views;
+  } catch (e) {
+    console.error(e);
+    return {};
+  }
 };
 
 export const saveError = async (error) => {
-	if (!error) {
-		return;
-	}
-	try {
-		const errorObj = {
-			message: error.message,
-			stacktrace: error.stack,
-		};
-		ErrorModel.parse(errorObj);
+  if (!error) {
+    return;
+  }
+  try {
+    const errorObj = {
+      message: error.message,
+      stacktrace: error.stack,
+    };
+    ErrorModel.parse(errorObj);
 
-		const savedError = prisma.error.create({ data: errorObj });
-	} catch (e) {
-		console.error(e);
-	}
+    const savedError = prisma.error.create({ data: errorObj });
+  } catch (e) {
+    console.error(e);
+  }
 };
