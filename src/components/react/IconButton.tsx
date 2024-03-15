d94268fd837a8872d9057f205b47ad77e0db609d
Aaron Lifton <aaronlifton@gmail.com>
2024-03-14 23:55:29 -0400

    add hamburger menu

--- src/components/react/IconButton.tsx
+++ src/components/react/IconButton.tsx
@@ -1,44 +1,47 @@
-import * as React from "react"
+import * as React from "react";
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
-interface Props extends React.ComponentPropsWithRef<'button'> {
-  variant?: string
+interface Props extends React.ComponentPropsWithRef<"button"> {
+  variant?: string;
 }
-const IconButton = React.forwardRef<HTMLButtonElement, Props>(({variant, className: providedClassName, ...rest}, ref) => {
-  variant ||= "rounded-full"
-  const className = React.useMemo(() => {
-     return twMerge(
-       clsx(
-         'bg-surface',
-         'hover:bg-hover',
-         'text-on-background',
-         'disabled:bg-disabled',
-         'disabled:text-on-disabled',
-         'disabled:hover:cursor-not-allowed',
-         'disabled:hover:bg-disabled',
-         'hover:text-primary',
-         'transition-colors',
-         'duration-300',
-         {
-           'min-w-[40px] h-[40px] flex justify-center items-center rounded-full px-2':
-             variant === 'rounded-full',
-           'rounded border border-divider p-1 shadow-sm shadow-shadow':
-             variant === 'rounded',
-         },
-         providedClassName
-       )
-     )
-  }, [variant, providedClassName])
+const IconButton = React.forwardRef<HTMLButtonElement, Props>(
+  ({ variant, className: providedClassName, ...rest }, ref) => {
+    variant ||= "rounded-full";
+    const className = React.useMemo(() => {
+      return twMerge(
+        clsx(
+          "bg-surface",
+          "hover:bg-hover",
+          "text-on-background",
+          "disabled:bg-disabled",
+          "disabled:text-on-disabled",
+          "disabled:hover:cursor-not-allowed",
+          "disabled:hover:bg-disabled",
+          "hover:text-primary",
+          "transition-colors",
+          "duration-300",
+          {
+            "min-w-[40px] h-[40px] flex justify-center items-center rounded-full px-2":
+              variant === "rounded-full",
+            "rounded border border-divider p-1 shadow-sm shadow-shadow":
+              variant === "rounded",
+            "hover:white bg:black": variant === "menu",
+          },
+          providedClassName,
+        ),
+      );
+    }, [variant, providedClassName]);
 
-   return (
-     <button
-       ref={ref as React.ForwardedRef<HTMLButtonElement>}
-       type='button'
-       {...rest}
-       className={className}
-     />
-   )
-})
+    return (
+      <button
+        ref={ref as React.ForwardedRef<HTMLButtonElement>}
+        type="button"
+        {...rest}
+        className={className}
+      />
+    );
+  },
+);
 
-export default IconButton
+export default IconButton;
