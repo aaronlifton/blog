d325f1e6e6c1fa6c9ac565f9b9cc9fa6d69a5f37
Aaron Lifton <aaronlifton@gmail.com>
2024-03-14 23:55:43 -0400

    add hamburger menu files

---
+++ src/util/menu.ts
@@ -0,0 +1,18 @@
+export const menuItems = [
+  {
+    name: "Home",
+    pathname: "/",
+  },
+  {
+    name: "Blog",
+    pathname: "/blog/1",
+  },
+  {
+    name: "Tags",
+    pathname: "/tags",
+  },
+  {
+    name: "About",
+    pathname: "/about",
+  },
+] as const;
