3bfc1f12ea0f2763dddf1dfe9e53c5c6cd2c08a5
Aaron Lifton <aaronlifton@gmail.com>
2024-03-31 22:15:10 -0400

    fix conflicts

--- src/services/trpc.ts
+++ src/services/trpc.ts
@@ -1,17 +1,10 @@
-import {createTRPCClient, createTRPCProxyClient, httpBatchLink} from "@trpc/client";
+import {
+	createTRPCClient,
+	createTRPCProxyClient,
+	httpBatchLink,
+} from "@trpc/client";
 import type { AppRouter } from "$rpc/router.ts";
 
-<<<<<<< HEAD
-const isLocalhost = import.meta.env.mode === "development";
-// Pass AppRouter as generic here. 👇 This lets the `trpc` object know
-// what procedures are available on the server and their input/output types.
-const trpc = createTRPCProxyClient<AppRouter>({
-  links: [
-    httpBatchLink({
-      url: isLocalhost ? "http://localhost:4333" : "http://localhost:4333",
-    }),
-  ],
-=======
 const isLocalhost = import.meta.env.MODE === "development";
 const trpc = createTRPCClient<AppRouter>({
 	links: [
@@ -19,7 +12,6 @@ const trpc = createTRPCClient<AppRouter>({
 			url: isLocalhost ? "http://localhost:4333" : "http://localhost:4333",
 		}),
 	],
->>>>>>> main
 });
 
 export default trpc;
