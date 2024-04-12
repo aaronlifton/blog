c65b3097fdf26c73e7a8181278c0eb00c57ecfed
Aaron Lifton <aaronlifton@gmail.com>
2024-04-12 12:24:22 -0400

    (chore) udpate prisma-libsql adapter

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -1,10 +1,10 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
 import { client as libsqlClient } from "$services/turso.ts";
+import { createClient } from "@libsql/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { PrismaClient } from "@prisma/client";
 
-export { MetricModel } from "$prisma/zod/metric";
 export { ErrorModel } from "$prisma/zod/error";
+export { MetricModel } from "$prisma/zod/metric";
 
 const adapter = new PrismaLibSQL(libsqlClient);
 export const prisma = new PrismaClient({ adapter });
