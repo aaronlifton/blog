a273ddfc571c0963290b371318f66de605515aa3
Aaron Lifton <aaronlifton@gmail.com>
2024-04-19 13:36:21 -0400

    (fix) language

--- src/services/blog.ts
+++ src/services/blog.ts
@@ -1,25 +1,21 @@
-import { getCollection } from "astro:content";
+import { type CollectionEntry, getCollection } from "astro:content";
 
-export async function getTags() {
-  const posts = await getCollection("blog");
+async function getLivePosts(): Promise<CollectionEntry<"blog">[]> {
+  return (await getCollection("blog")).filter((post) => !post.data.draft);
+}
+
+export async function getTags(): Promise<string[]> {
+  const posts = await getLivePosts();
   return [...new Set(posts.flatMap((post) => post.data.tags))];
 }
 
-export async function getPosts() {
-  return (await getCollection("blog"))
-    .filter((post) => !post.data.draft)
+export async function getPosts(): Promise<CollectionEntry<"blog">[]> {
+  return (await getLivePosts())
     .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
 }
 
-export async function getPostsByTag(tag: string) {
-  return (await getCollection("blog"))
-    .filter((post) => !post.data.draft)
+export async function getPostsByTag(tag: string): Promise<CollectionEntry<"blog">[]> {
+  return (await getLivePosts())
     .filter((post) => post.data.tags.includes(tag))
     .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
 }
-
-// export async function getGuides() {
-//   return (await getCollection("guides"))
-//     .filter((guide) => guide.data.published)
-//     .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
-// }
