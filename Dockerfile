92437370a4bfeb859c3c48288ffbafc5640fd1da
Aaron Lifton <aaronlifton@gmail.com>
2024-04-27 01:38:14 -0400

    fix: add RENDER_TOKEN to env

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,8 @@ SHELL ["/bin/ash", "-o", "pipefail", "-c"]
 RUN --mount=type=secret,id=env,dst=/etc/secrets/.env \
       TURSO_DB_AUTH_TOKEN=$(grep TURSO_DB_AUTH_TOKEN /etc/secrets/.env | cut -d '=' -f 2) \
       TURSO_DB_URL=$(grep TURSO_DB_URL /etc/secrets/.env | cut -d '=' -f 2) \
-      export TURSO_DB_URL && export TURSO_DB_AUTH_TOKEN \
+      RENDER_TOKEN=$(grep RENDER_TOKEN /etc/secrets/.env | cut -d '=' -f 2) \
+      export TURSO_DB_URL && export TURSO_DB_AUTH_TOKEN && export RENDER_TOKEN\
       && cp /etc/secrets/.env .env \
       && npm install \
       && npm run prisma-generate \
--- src/pages/api/ops/render.ts
+++ src/pages/api/ops/render.ts
@@ -17,14 +17,18 @@ type ApiResponse = {
   numDeploysInPast7Days: number;
 };
 
+const BASE_URL = "https://api.render.com/v1";
+const TOKEN = import.meta.env.RENDER_TOKEN;
 const getDeploys: () => Promise<ApiResponse> = async () => {
-  const url =
-    "https://api.render.com/v1/services/srv-cnl30lmd3nmc73evs8kg/deploys?limit=20";
+  if (!TOKEN) {
+    throw new Error("RENDER_TOKEN is not defined");
+  }
+  const url = `${BASE_URL}/services/srv-cnl30lmd3nmc73evs8kg/deploys?limit=20`;
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
-      authorization: "Bearer rnd_9c0P3AJgG5VlRpjQd4WnyrmUJJ2W",
+      authorization: `Bearer ${TOKEN}`,
     },
   };
 
@@ -39,8 +43,7 @@ const getDeploys: () => Promise<ApiResponse> = async () => {
 export const GET: APIRoute = async (Astro) => {
   const data = await getDeploys();
   const numDeploysInPast7Days = data.deploys?.filter(
-    (deploy) =>
-      new Date(deploy.deploy.createdAt).getSeconds() > Date.now() - 7 * 24 * 60 * 60,
+    (deploy) => new Date(deploy.deploy.createdAt).getSeconds() > Date.now() - 7 * 24 * 60 * 60,
   )?.length;
   return new Response(JSON.stringify({ deploys: data.deploys, numDeploysInPast7Days }));
 };
