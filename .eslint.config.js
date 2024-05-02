2d2982722bc34b5dcfa80a999a2b97d10b077d1b
Aaron Lifton <aaronlifton@gmail.com>
2024-05-02 17:43:24 -0400

    📦️ deps(eslint): add eslint astro config

---
+++ .eslint.config.js
@@ -0,0 +1,15 @@
+import eslintConfig from "@azat-io/eslint-config-astro";
+
+export default eslintConfig;
+// import eslintPluginAstro from "eslint-plugin-astro";
+// export default [
+//   // add more generic rule sets here, such as:
+//   // js.configs.recommended,
+//   ...eslintPluginAstro.configs.recommended,
+//   {
+//     rules: {
+//       // override/add rules settings here, such as:
+//       // "astro/no-set-html-directive": "error"
+//     },
+//   },
+// ];
