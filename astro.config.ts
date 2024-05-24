caac7f74c439ba3efe8183298042a192bb499fd9
Aaron Lifton <aaronlifton@gmail.com>
2024-05-23 21:47:16 -0400

    Update TOC scroller ball logic

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,4 @@
 import mdx from "@astrojs/mdx";
-import path, { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-// import solid from "@astrojs/solid-js";
 import node from "@astrojs/node";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -13,9 +10,12 @@ import {
   transformerNotationWordHighlight,
 } from "@shikijs/transformers";
 import { transformerTwoslash } from "@shikijs/twoslash";
+import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig, squooshImageService } from "astro/config";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { remarkShakuCodeAnnotate } from "remark-shaku-code-annotate";
 import remarkShikiTwoSlash from "remark-shiki-twoslash";
@@ -23,22 +23,12 @@ import svgr from "vite-plugin-svgr";
 import customImageResizer from "./bin/processImages.mjs";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time";
 import { remarkSummary } from "./src/plugins/remark-summary";
-// import {
-// transformerNotationDiff,
-// transformerNotationErrorLevel,
-// transformerNotationFocus,
-// transformerNotationHighlight,
-// } from "shikiji-transformers";
-import expressiveCode from "astro-expressive-code";
-// import wasm from "vite-plugin-wasm";
-// import topLevelAwait from "vite-plugin-top-level-await";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /**
  * @type {import('astro').AstroConfig}
  */
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({
@@ -49,21 +39,7 @@ export default defineConfig({
   integrations: [
     customImageResizer,
     expressiveCode(),
-    mdx({
-      // remarkPlugins: [
-      //   remarkReadingTime,
-      //   // @ts-expect-error - This is a valid plugin that returns a Transformer,
-      //   // but the type definition is not compatible with the expected [unified.Plugin, string[]] type.
-      //   // remarkPlugins accepted both unified.Plugin, [unified.Plugin, string[]] and [unified.Plugin, { [key: string]: any }]
-      //   [
-      //     remarkShikiTwoSlash.default,
-      //     {
-      //       theme: "material-darker",
-      //     },
-      //   ],
-      //   remarkSummary,
-      // ],
-    }),
+    mdx(),
     sitemap(),
     tailwind({
       applyBaseStyles: false,
@@ -71,9 +47,6 @@ export default defineConfig({
     }),
     svelte(),
     icon(),
-    // solid({
-    // include: ["**/solid/*"],
-    // }),
     react({}),
     robotsTxt(),
     sitemap(),
@@ -123,22 +96,9 @@ export default defineConfig({
     shikiConfig: {
       theme: "material-theme-darker",
       transformers: [
-        // {
-        // code(node) {
-        // this.addClassToHast(node, "language-js");
-        // },
-        // line(node, line) {
-        // node.properties["data-line"] = line;
-        // if ([1, 3, 4].includes(line))
-        // this.addClassToHast(node, "highlight");
-        // },
-        // span(node, line, col) {
-        // node.properties["data-token"] = `token:${line}:${col}`;
-        // },
-        // },
         transformerNotationDiff(),
         transformerNotationHighlight(),
-        // transformerNotationWordHighlight(),
+        transformerNotationWordHighlight(),
       ],
     },
     extendDefaultPlugins: true,
@@ -154,8 +114,6 @@ export default defineConfig({
       },
     },
     plugins: [
-      // wasm(),
-      // topLevelAwait(),
       svgr({
         include: "**/*.svg?react",
         svgrOptions: {
