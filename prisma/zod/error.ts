ab2a21963149e5c7de8fd7847888b557c8b973fc
Aaron Lifton <aaronlifton@gmail.com>
2024-05-28 17:10:44 -0400

    🔧 chore(integration-tests): remove unused integration test experiments (Cypress, Nightwatch)
    These have been replaced with Playwright

--- prisma/zod/error.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod";
-
-export const ErrorModel = z.object({
-  id: z.string(),
-  message: z.string(),
-  stackTrace: z.string(),
-  createdAt: z.date(),
-});
--- prisma/zod/metric.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod";
-
-export const MetricModel = z.object({
-  id: z.string(),
-  postSlug: z.string(),
-  value: z.bigint(),
-  metricType: z.string(),
-});
